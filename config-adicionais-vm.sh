2481c830e825266932b9adf06ef0fb57c86a2405
Alessandro Rodrigues <arodriguesdoria@gmail.com>
2022-12-13 22:39:34 -0300

    Docker Cluster DIO v.1

---
+++ config-adicionais-vm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Iniciando configurações....."
+echo "Iniciada instalação do Docker!"
+
+curl -fsSl https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+echo "Finalizada instalação do Docker!"
---
+++ worker.sh
@@ -0,0 +1,1 @@
+    docker swarm join --token SWMTKN-1-33dhkg12sd954xmsufw615l4j8lfhma114w0hxhjq88d1bgjqu-7moh1mh8ltsgwmli0nmt9ouqo 192.168.6.100:2377
